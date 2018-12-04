6f78da6c59e426718e2df845ca046ec21cd17d5d
theoflores <theo.flores@imie.fr>
2018-12-04 11:44:24 +0100

    Création d'une class person avec ses instances

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
-var_dump(expression:"coucou");
+class Person {
+    private $firstname;
+    private $lastname;
+
+    public function setFirstname($pFirstname){
+        $this->firstname = $pFirstname;
+    }
+    public function setLastname($pLastname){
+        $this->lastame = $pLastname;
+    }
+}
+    
+$person1 = new Person();
+//$person1->firstname = "Toto";
+//$person1->lastname = "Labricot";
+$person1->setFirstname("Toto");
+$person1->setLastname("Labricot");
+
+
+var_dump($person1);
+
+//$person2 = new Person();
+//$person2->firstname = "Stefan";
+//$person2->lastname = "Lelephant";
+//var_dump($person2);
