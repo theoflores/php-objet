df189ca564f33c79d3f6332e39b0d91a37fe9ac5
theoflores <theo.flores@imie.fr>
2018-12-04 12:13:11 +0100

    Ajouter la méthode speak

--- index.php
+++ index.php
@@ -1,27 +1,51 @@
 <?php
-
 class Person {
     private $firstname;
     private $lastname;
-
-    public function setFirstname($pFirstname){
+    /**
+     * Création d'un setter (ou mutateur)
+     *
+     * @param $pFirstname
+     */
+    public function setFirstname($pFirstname) {
         $this->firstname = $pFirstname;
     }
-    public function setLastname($pLastname){
-        $this->lastame = $pLastname;
+    /**
+     * Création d'un getter (ou accesseur)
+     *
+     * @return mixed
+     */
+    public function getFirstname() {
+        return $this->firstname;
+    }
+    public function setLastname($pLastname) {
+        $this->lastname = $pLastname;
+    }
+    public function getLastname() {
+        return $this->lastname;
+    }
+    public function speak() {
+        return "Hello my name is " . $this->firstname . " " . $this->lastname ;
     }
 }
-    
 $person1 = new Person();
-//$person1->firstname = "Toto";
-//$person1->lastname = "Labricot";
+//$person1->firstname = "John";
+//$person1->lastname = "Doe";
+
+
 $person1->setFirstname("Toto");
-$person1->setLastname("Labricot");
+$person1->setLastname("Labrico");
+var_dump($person1->speak());
 
 
-var_dump($person1);
+
+//var_dump($person1->getFirstname());
+//var_dump($person1->getLastname());
 
 //$person2 = new Person();
-//$person2->firstname = "Stefan";
-//$person2->lastname = "Lelephant";
+//$person2->firstname = "Jane";
+//$person2->lastname = "Die";
+//$person2->setFirstname("Jane");
+//$person2->setLastname("Die");
+//
 //var_dump($person2);
